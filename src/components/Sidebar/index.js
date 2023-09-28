f56f322fb220d2c7b4589f9f297c061ea954c49d
pprometheus <67922206+pprometheus@users.noreply.github.com>
2023-09-28 12:16:00 +0530

    Updated Sidebar
    Sidebar - Added Logo , Navigation and Social Links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,16 +1,52 @@
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHome,faUser,faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {faBehance, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
+
 
 const Sidebar = () => (
 
     <div className='nav-bar'>
     <Link className='logo' to='/'>
         <img src={LogoS} alt='logo'/>
-        <img src={LogoSubtitle} alt='amith'/>
+        <img className="sub-logo" src={LogoSubtitle} alt='amith'/>
 
     </Link>
+    <nav>
+        <NavLink exact="true" activeclassname="active" to="/" >
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+        </NavLink>
+         
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about " >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+        </NavLink>
+
+        <NavLink exact="true"activeclassname="active" className="contact-link" to="/contact" >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+        </NavLink>
+    </nav>
+    <ul>
+        <li>
+            <a target='blank' rel='noreferrer' href='https://www.linkedin.com/in/amithhatwar'>
+                <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e'/>
+            </a>
+        </li>
+
+        <li>
+            <a target='blank' rel='noreferrer' href='https://github.com/pprometheus'>
+                <FontAwesomeIcon icon={faGithub} color='#4d4d4e'/>
+            </a>
+        </li>
+
+        <li>
+            <a target='blank' rel='noreferrer' href='https://www.behance.net/amithhatwar'>
+                <FontAwesomeIcon icon={faBehance} color='#4d4d4e'/>
+            </a>
+        </li>
+    </ul>
     </div>
 )
 
