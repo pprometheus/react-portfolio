9961a53e65206e4bfc946f99bbc3309bde405858
pprometheus <67922206+pprometheus@users.noreply.github.com>
2023-09-29 11:10:44 +0530

    Home Page Design

---
+++ src/components/Home/index.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import './index.scss';
+const Home =() =>{
+
+    return(
+      <div className="container home-page">
+           <div className="text-zone">
+             <h1>Hi,<br/>I'm
+             <img src={LogoTitle} alt="developer" />
+            lobodan
+            <br/>
+            web developer
+            </h1>
+            <h2>Frontend Developer/ UI/UX Designer / Graphic Designer</h2>
+           <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+           </div>
+      </div>  
+    );
+}
+
+export default Home;
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,28 @@
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar';
 import './index.scss';
 
 const Layout = () =>{
     return(
-        <>
+        <div className='App'>
         <Sidebar/>
-        </>
-    );
+        <div className='page'>
+            <span className='tags top-tags'>
+                &lt;body&gt;
+            </span>
+
+            <Outlet/>
+
+            <span className='tags bottom-tags'>
+                &lt;body&gt;
+            </span>
+
+            <span className='tags bottom-tag-html'>
+                &lt;/html&gt;
+            </span>
+
+        </div>
+        </div>
+            );
 }
 export default Layout
