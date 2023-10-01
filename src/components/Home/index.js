38c6d202bfe4ea38ab28b009131025fadeea2983
pprometheus <67922206+pprometheus@users.noreply.github.com>
2023-10-01 21:32:55 +0530

    Home page update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,45 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
-const Home =() =>{
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
+  const Home =() =>{
+  const [letterClass,setLetterClass] = useState('text-animate')
+  const nameArray = ['l','o','b','o','d','a','n']
+  const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r']
+  
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+  
+    return () => clearTimeout(timeoutId);
+  }, []); 
+  
 
-    return(
+  return(
       <div className="container home-page">
            <div className="text-zone">
-             <h1>Hi,<br/>I'm
+             <h1>
+              <span className={letterClass}>H</span>
+              <span className={`${letterClass} _12`}>i,</span>
+              <br/>
+              <span className={`${letterClass} _13`}>I</span>
+              <span className={`${letterClass} _14`}>'m </span>
              <img src={LogoTitle} alt="developer" />
-            lobodan
+            <AnimatedLetters letterClass={letterClass}
+            strArray={nameArray}
+            idx={15} />
             <br/>
-            web developer
+            <AnimatedLetters letterClass={letterClass}
+            strArray={jobArray}
+            idx={22} />
             </h1>
             <h2>Frontend Developer/ UI/UX Designer / Graphic Designer</h2>
            <Link to="/contact" className='flat-button'>CONTACT ME</Link>
            </div>
+           <Logo/>
       </div>  
     );
 }
